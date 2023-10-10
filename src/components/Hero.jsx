e88a636502673ed33c099048ed79a4df3f5cc3b4
aproperzi2 <abramoproperzi@gmail.com>
2023-10-09 20:36:29 -0600

    added google analytics in header

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -29,7 +29,7 @@ const Hero = () => {
             </span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            We develop custom <br className="sm:block hidden" />
+            We develop custom design, <br className="sm:block hidden" />
             websites and software.
           </p>
         </div>
